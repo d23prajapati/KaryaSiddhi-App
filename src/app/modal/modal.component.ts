f401352d688483b1a6bec02f045e8a69b97ba793
darshan prajapati <d23prajapati@gmail.com>
2023-03-10 16:06:17 +0530

    initial commit

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,7 +1,6 @@
 import { Component, Output, OnInit, EventEmitter, Input, OnChanges } from '@angular/core';
 import { TaskService } from '../Services/task.service';
 import { Task } from '../Task';
-import {Observable} from 'rxjs'
 
 @Component({
   selector: 'app-modal',
@@ -27,10 +26,6 @@ export class ModalComponent implements OnInit, OnChanges{
 
   }
 
-  // closeModal() {
-  //   this.showModel = !this.showModel;
-  // }
-
   onCrossBtnClick() {
     this.closeModalEvent.emit(false);
   }
@@ -43,19 +38,17 @@ export class ModalComponent implements OnInit, OnChanges{
     status: ""
   };
   onTaskCreate(data: { title:string, description:string, priority:string, duedate: string}) {
-    console.log(data);
     this.closeModalEvent.emit(false);
     this.task.title = data.title;
     this.task.description = data.description;
     this.task.priority = data.priority;
     this.task.dueDate = data.duedate;
     this.task.status = "incomplete";
-    this.task.taskId = "0";
+    this.task.taskId = "00";
 
     // console.log(this.task);
     this.taskService.postTask(this.task).subscribe((res) => {
       this.ngOnInit();
     });
-
   }
 }
